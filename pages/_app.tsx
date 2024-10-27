d8858f4f5f758c6291c10cba41b7850296faca86
NicholasGoh <gohn0004@e.ntu.edu.sg>
2024-10-27 14:23:09 +0800

    format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 const inter = Inter({ subsets: ["latin"] });
 const queryClient = new QueryClient();
-const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY || '';
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY || "";
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
